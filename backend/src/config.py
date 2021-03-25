e5e6950e8c1a67260d5af8e85e1ebc21827ee223
Danyal Soomro <danyal.s.94@gmail.com>
2021-03-25 21:57:20 +0500

    working structure

---
+++ backend/src/config.py
@@ -0,0 +1,29 @@
+import os
+import binance.config as binance
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    BINANCE_API_SECRET = binance.API_SECRET
+    BINANCE_API_KEY = binance.API_SECRET
+    
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,18 @@
 import time
 import datetime
+from flask import Flask
+app = Flask(__name__)
 
 def main():
-    while True:
-        time.sleep(5)
-        print(f"[{datetime.datetime.now().isoformat()}] Running")
-        
+    app.run(host='0.0.0.0', port=5001)
+
+@app.route('/')
+def hello():
+    return {
+                "message":"Hello World!"
+           }
 
 if __name__ == "__main__":
     main()
+
+# %%
