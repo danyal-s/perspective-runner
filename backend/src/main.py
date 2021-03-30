f78987bd3862ccb68ec4213f8f862a4a755e082d
Danyal Soomro <danyal.s.94@gmail.com>
2021-03-30 09:24:36 +0500

    Added some basic functionality

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,18 @@
 # %%
 import time
 import datetime
-from flask import Flask
+from flask import Flask, Blueprint
+from flask_restx import Api, Resource, fields
 import config
 import os
 from binance.client import Client
 import requests
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 APP_CONFIG = None
 
 if os.environ.get("APP_ENV", "dev") in ("dev", "prod"):
@@ -14,29 +21,217 @@ if os.environ.get("APP_ENV", "dev") in ("dev", "prod"):
 b_client = Client(APP_CONFIG.BINANCE_API_KEY, APP_CONFIG.BINANCE_API_SECRET)
 app = Flask(__name__)
 
-def main():
-    app.run(host='0.0.0.0', port=APP_CONFIG.BACKEND_API_PORT)
+bp = Blueprint('api', __name__, url_prefix="/api")
+api = Api(  bp, 
+            version='1.0', 
+            title='Backend API',
+            description='This should get all my crypto data')
+app.register_blueprint(bp)
 
-@app.route('/')
-def hello():
-    return {
-                "message":"Hello World!"
-           }
+class CryptoExternalAPIStore(object):
 
-@app.route("/binance_assets")
+    def __init__(self, api_call_delay=60):
+        self.time = time.time()
+        self.results_store = {}
+        self.api_delay = api_call_delay
+
+
+    def get_from_external_api(self, url):
+        return requests.get(url)
+
+
+    def _set_results_store(self, url, r):
+        if url not in self.results_store.keys():
+            self.results_store[url] = {}
+
+        self.results_store[url]["retrieval_time"] = time.time()
+        self.results_store[url]["result"] = r
+
+
+    def _get_from_results_store(self, url):
+        return self.results_store[url]
+
+    def _process_result(self, r):
+        return r.json()
+
+    def get(self, url):
+        if url in self.results_store.keys():
+            if time.time() - self._get_from_results_store(url)["retrieval_time"] > self.api_delay:
+                try:
+                    r = self.get_from_external_api(url)
+                except Exception as e:
+                    return None
+
+                self._set_results_store(url, r)
+
+            else:
+                r = self._get_from_results_store(url)["result"]
+        else:
+            try:
+                r = self.get_from_external_api(url)
+                self._set_results_store(url, r)
+            except Exception as e:
+                logger.exception(e)
+                return None
+         
+        return self._process_result(r)
+
+class BinanceCryptoExternalAPIStore(CryptoExternalAPIStore):
+    def __init__(self, api_call_delay=None, b_client=None):
+        if api_call_delay is None:
+            api_call_delay = 60
+        if b_client is None:
+            raise ValueError("Required parameter b_client not passed")
+    
+        super().__init__(api_call_delay)
+        self.b_client = b_client
+
+    def get_from_external_api(self, action):
+        if action == "get_crypto_prices":
+            res = self.b_client.get_all_tickers()
+
+        return res
+    
+    def _process_result(self, r):
+        return r
+
+b_client.get_all_tickers()
+
+external_api_res_store = CryptoExternalAPIStore(30)
+binance_api_res_store = BinanceCryptoExternalAPIStore(10, b_client=b_client)
+
+
+def get_all_crypto_prices():
+    crypto_prices = binance_api_res_store.get("get_crypto_prices")
+    for di in crypto_prices:
+        di["price"] = float(di["price"])
+    
+    return crypto_prices
+
+
+def get_crypto_price(crypto_symbol, liquid_cond=None):
+    if liquid_cond is None:
+        liquid_cond = lambda crypto_symbol, x: crypto_symbol in x["symbol"] and (("USDC" in x["symbol"]) or ("USDT" in x["symbol"]))
+
+    crypto_prices = get_all_crypto_prices()
+    price_data = float([x for x in crypto_prices if liquid_cond(crypto_symbol, x)][0]["price"])
+    
+    return price_data
+
+
+def get_binance_cryptos():
+    crypto_details = [d for d in b_client.get_account()["balances"] if float(d["free"]) > 0]
+    for di in crypto_details:
+        di["free"] = float(di["free"])
+
+    return crypto_details
+
 def get_binance_assets():
-    return {"res": [d for d in b_client.get_account()["balances"] if float(d["free"]) > 0]}
+    crypto_details = [d for d in b_client.get_account()["balances"] if float(d["free"]) > 0]
+    for c in crypto_details:
+        try:
+            if c["asset"] in ("USDC", "USDT", "DAI"):
+                c["current_price"] = 1.0
+            elif c["asset"] == "WBTC":
+                c["current_price"] = get_crypto_price("WBTC", lambda _, x: "WBTCBTC" in x["symbol"]) * get_crypto_price("BTC")
+            else:
+                c["current_price"] = get_crypto_price(c["asset"])
+            c["amount_in_usd"] = c["current_price"]*float(c["free"])
+        except IndexError as e:
+            logger.exception(f"{c['asset']} price not found: {e}")
+            c["current_price"] = None
+    return crypto_details
 
 
-def get_all_eth_transactions():
-    r = requests.get(f"https://api.etherscan.io/api?module=account&action=txlist&address=0xbA431c3f9B6dFB27450616506B330E3Dd5f72E0a&startblock=12082330&endblock=99999999&sort=asc&apikey={APP_CONFIG.ETHERSCAN_API_KEY}")
-    eth_transactions = r.json()["result"]
-    return {
-            "res": eth_transactions
-            }
+def get_pancake_lp_stats():
+    r = external_api_res_store.get(f"https://www.yieldwatch.net/api/all/0xbA431c3f9B6dFB27450616506B330E3Dd5f72E0a?platforms=pancake")
+    pancake_lps = r["result"]["PancakeSwap"]
+    vault_stats = []
+    lp_stats = pancake_lps["LPStaking"]
+    vaults = lp_stats["vaults"]
+    lp_usd_stats = lp_stats["totalUSDValues"]
+    for v in vaults:
+        vault_stats.append({
+            "poolName": v["name"],
+            "rewardToken": v["rewardToken"],
+            "pendingRewards": v["pendingRewards"],
+            "harvestedRewards": v["harvestedRewards"],
+            "totalEarnedFromRewards": v["totalRewards"]*v["priceInUSDRewardToken"],
+            "rewardToken": v["rewardToken"],
+            "poolTransactions": v["LPInfo"]["compactSessions"],
+            "token0": v["LPInfo"]["symbolToken0"],
+            "token1": v["LPInfo"]["symbolToken1"],
+            "token0price": v["LPInfo"]["priceInUSDToken0"],
+            "token1price": v["LPInfo"]["priceInUSDToken1"],
+            'currentToken0Count': v["LPInfo"]["currentToken0"],
+            'currentToken1Count': v["LPInfo"]["currentToken1"],
+            'depositToken0Count': v["LPInfo"]["depositToken0"],
+            "depositToken1Count": v["LPInfo"]["depositToken1"],
+            'changeToken0Count': v["LPInfo"]["changeToken0"],
+            'changeToken1Count': v["LPInfo"]["changeToken1"],
+            'actPrice': v["LPInfo"]["actPrice"],
+            'depositPrice': v["LPInfo"]["depositPrice"],
+            'faktorIL': v["LPInfo"]["faktorIL"],
+            'ILInToken1': v["LPInfo"]["ILInToken1"],
+            #'ILInPerc': v["LPInfo"]["ILInPerc"],
+            #'LPEarningsInToken1': v["LPInfo"]['LPEarningsInToken1'],
+            #'LPEarningsInPerc': v["LPInfo"]['LPEarningsInPerc'],
+            #'feesEarnedInToken0': v["LPInfo"]["feesEarnedInToken0"],
+            #'feesEarnedInToken1': v["LPInfo"]['feesEarnedInToken1'],
+            #'feesEarnedInPerc': v["LPInfo"]['feesEarnedInPerc']
+    })
+    res = {
+        "deposited_money": lp_usd_stats["deposit"],
+        "yield_money": lp_usd_stats["yield"],
+        "total_money": lp_usd_stats["total"],
+        "vaultStats": vault_stats
+    }
+    return res
+
+
+@api.route('/')
+class Hello(Resource):
+    def get(self): 
+        return {"message":"Hello World!"}
+
+
+@api.route("/binance_assets")
+class GetBinanceAssets(Resource):
+    def get(self):
+        crypto_details = get_binance_assets()
+        return {"res": crypto_details}
+
+
+@api.route("/eth_transactions")
+class GetAllEthTransactions(Resource):
+    def get(self):
+        r = external_api_res_store.get("https://api.etherscan.io/api?module=account&action=txlist&address=0xbA431c3f9B6dFB27450616506B330E3Dd5f72E0a&startblock=12082330&endblock=99999999&sort=asc&apikey={APP_CONFIG.ETHERSCAN_API_KEY}")
+        eth_transactions = r.json()["result"]
+        return {"res": eth_transactions}
+
+
+@api.route("/get_pancake_liquidity_pool")
+class GetPancakeLiquidityPool(Resource):
+    def get(self):
+        res = get_pancake_lp_stats()
+        return {"res": res}
+
+
+@api.route("/get_total_assets")
+class GetTotalAssets(Resource):
+    def get(self):
+        total_usd = 0
+        bin_assets = get_binance_assets()
+        for x in bin_assets:
+            total_usd += x["current_price"]
+        
+        p_lp_stats = get_pancake_lp_stats()
+        
+        return {"res": {"total_usd": total_usd}}
+
+
+def main():
+    app.run(host='0.0.0.0', port=APP_CONFIG.BACKEND_API_PORT, debug=True)
 
 if __name__ == "__main__":
     main()
-
-# %%
-# %%
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,63 +1,109 @@
 <script>
-	import { onMount } from "svelte";
+	import { onMount, onDestroy } from "svelte";
 
 	export let name;
-	let the_data = null;
+	let scheduler = null;
+	let binance_assets = null;
+	let pancake_lp = null;
+
+	async function get_api_data () {
+		var res = await fetch("api/binance_assets");
+    	binance_assets = await res.json();
+		binance_assets = binance_assets["res"]
+		
+		res = await fetch("api/get_pancake_liquidity_pool");
+		pancake_lp = await res.json();
+		pancake_lp = pancake_lp["res"]
+	}
+
 	onMount(async () => {
-		const res = await fetch("api/binance_assets");
-    	the_data = await res.json();
-		the_data = the_data["res"]
+		get_api_data()
+		if (scheduler == null) {
+			scheduler = setInterval(get_api_data, 5000)
+		}
 	})
 
-</script>
-
-<main>
-	<section class="section">
-		<div class="container">
-		  <h1 class="title">
-			Hello World
-		  </h1>
-		  <p class="subtitle">
-			My first website with <strong>Bulma</strong>!
-		  </p>
-		</div>
-	  </section>
-	<h1>Hello {name}!</h1>
-	<h2>Here data: {JSON.stringify(the_data)}</h2>
+	onDestroy(() => clearInterval(scheduler));
+ 
 	
-	<table>
-		<th>Symbol</th>
-		<th>Amount</th>
-			{#if the_data != null}
-				{#each the_data as k}
-				<tr>
-					<td>{k.asset}</td>
-					<td>{k.free}</td>
-				</tr>
-				{/each}
-			{/if}
-	</table>
+
+</script>
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+		<a class="navbar-brand" href="#">Crypto App</a>
+        <ul class="navbar-nav w-100">
+            <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Link</a></li>
+            <!--<li class="nav-item ml-md-auto"><a href="#" class="btn btn-primary switch">Switch Light/Dark</a></li>-->
+        </ul>
+	</nav>
+	<section class="container-fluid">
+		{JSON.stringify(pancake_lp)}		
+		
+	
+	<div class="row">
+		<div class="col-md-4">
+			<h2>Binance Info</h2>
+			<table class="table table-dark">
+				<th>Symbol</th>
+				<th>Amount</th>
+				<th>USD Value of Token</th>
+				<th>USD Value Held</th>
+					{#if binance_assets != null}
+						{#each binance_assets as k}
+						<tr>
+							<td>{k.asset}</td>
+							<td>{k.free}</td>
+							<td>${k.current_price.toFixed(2)}</td>
+							<td>${k.amount_in_usd.toFixed(2)}</td>
+						</tr>
+						{/each}
+					{:else}
+						<tr>
+							<td>Loading...</td>
+						</tr>
+					{/if}
+			</table>
+		</div>
+		<div class="col-md-3">
+			<div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+				<div class="card-header">Pancake Swap LP</div>
+				<div class="card-body">
+				{#if pancake_lp != null}
+					<div class="row">
+						<div class="col-md-6 text-center">
+							<h5 class="card-title">Deposited</h5>
+							<p class="card-text">${pancake_lp.deposited_money.toFixed(2)}</p>
+						</div>
+						<div class="col-md-6 text-center">
+							<h5 class="card-title">Yielded</h5>
+							<p class="card-text">${pancake_lp.yield_money.toFixed(2)}</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12 text-center">
+							<h5 class="card-title">Total</h5>
+							<p class="card-text">${pancake_lp.total_money.toFixed(2)}</p>
+						</div>
+				</div>
+				  {#each pancake_lp.vaultStats as p}
+				  <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+				  	<div class="card-header">{p.poolName}</div>
+					<div class="card-body">
+						<p class="card-text">{p.token0} count: {p.currentToken0Count.toFixed(6)}</p>
+						<p class="card-text">Change in count:  {(((p.currentToken0Count - p.depositToken0Count) / p.depositToken0Count)*100).toFixed(2)}%</p>
+						<p class="card-text">{p.token1} count: {p.currentToken1Count}</p>
+						<p class="card-text">Change in count:  {(((p.currentToken1Count - p.depositToken1Count) / p.depositToken1Count)*100).toFixed(2)}%</p>
+						
+					</div>
+				</div>
+				  {/each}
+				{:else}
+					<p>Loading...</p>
+				{/if}
+				</div>
+			  </div>
+		</div>
+	</div>
 	<p></p>
-</main>
+</section>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
